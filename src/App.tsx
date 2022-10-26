4e2acc467816f4086cb465aa8ab9e4e0b859c993
Zammy <dthar06@gmail.com>
2022-10-26 23:43:58 +0700

    modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  return <div className="bg-red-400 h-screen"></div>;
+  return <div className="bg-red-400 h-screen">I'm here Kwee</div>;
 }
 
 export default App;
