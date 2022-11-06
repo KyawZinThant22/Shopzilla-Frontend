46b0e194fa58085137d3b14fdbdfaa47b485ed63
KyawZinThant22 <kyawthantver2@gmail.com>
2022-11-06 17:48:44 +0630

    folder

--- src/section/Home/Home.tsx
+++ src/section/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Helmet } from '../../Components/Element';
 import Hero from './Hero';
+import ProductSection from './ProductSection';
 
 const Home = () => {
   return (
     <>
       <Helmet title="Home Page" />
       <Hero />
+      <ProductSection />
     </>
   );
 };
