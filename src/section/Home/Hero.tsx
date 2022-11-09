67c8fc0407410ae903caaf0c2f5ef5e32b412596
KyawZinThant22 <kyawthantver2@gmail.com>
2022-11-09 21:17:40 +0630

    type for image

--- src/section/Home/Hero.tsx
+++ src/section/Home/Hero.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import chair from '../../assets/products/chair.png';
 
 const Hero = () => {
   return (
-  <div className="min-w-full min-h-screen bg-[#FBEEE6] absolute top-0 left-0 z-0">
-    <div className="grid grid-cols-2 p-52">
-      <h1>Hello World</h1>
-      <h1>Hello Zarmani</h1>
+    <div className="min-w-full min-h-screen bg-[#FBEEE6] absolute top-0 left-0 z-0">
+      <div className="grid grid-cols-2 p-52">
+        <h1>Hello World</h1>
+        <h1>Hello Zarmani</h1>
+        <img src={chair} alt="chair" />
+      </div>
     </div>
-  </div>
   );
 };
 
